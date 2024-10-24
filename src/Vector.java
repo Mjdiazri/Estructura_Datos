3fd6bc98f6288a90e1f51b52fec29f67ba9fde29
Mjdiazri <citruss2012@hotmail.es>
2024-10-23 20:30:35 -0500

    Se agrega metodo de eliminar, adicionar en posicion, validaciones,

--- src/Vector.java
+++ src/Vector.java
@@ -4,6 +4,7 @@ public class Vector {
     private Estudiantes[] alumnos = new Estudiantes[100];
     private int totalEstudiantes = 0;
 
+        //Metodo agregar estudiante: Agrega estudiante en orden
         public void agregar (Estudiantes estudiante){
             //OPCION 1
 //            for (int i = 0; i < alumnos.length; i++) {
@@ -18,21 +19,75 @@ public class Vector {
             totalEstudiantes ++;
         }
 
-        public  Estudiantes obtener (int posicion){
-            return null;
+
+
+        //Metodo posicionOcupada:  Indica los parametros para que un nombre "exista" en el array (Que la posicion sea +0 y <al total de estudicantes )
+        private boolean posicionOcupada(int posicion){
+            return posicion >= 0 && posicion < totalEstudiantes;
         }
+
+
+
+        //Metodo para encontrar el nombre de un estudiante por su posicion en el array
+        public  Estudiantes obtener (int posicion){
+            //Con excepciones
+            if(!posicionOcupada(posicion)){
+                throw new IllegalArgumentException("Posicion no encontrada");
+            }
+            return alumnos[posicion];
+        }
+
+
+        //Metodo para eliminar un estudiante
 
         public  void eliminar (int posicion){
+            for (int i = posicion; i < this.totalEstudiantes ; i++) {
+                this.alumnos[i] = this.alumnos[i+1];
+            }
 
+            totalEstudiantes --;
         }
 
+
+        //Metodo para validar si un estudiante ya existe en el array
         public boolean contiene(Estudiantes estudiante){
+            for (int i = 0; i < totalEstudiantes; i++) {
+                if(estudiante.equals(alumnos[i])){
+                    return true;
+                }
+            }
             return false;
         }
 
+
+
+        //Metodo para establecer el tamaño del array. Solo incluye nombres
         public int tamanio(){
             return totalEstudiantes;
         }
+
+
+
+        // *************** Metodo agregar en cualquier posicion*********************
+
+        private boolean posicionValida(int posicion){
+            return posicion >= 0 && posicion <= totalEstudiantes;
+        }
+
+        public void adiciona(int posicion, Estudiantes estudiante){
+
+            if(!posicionValida(posicion)){
+                throw new IllegalArgumentException("Posicion invalida");
+            }
+
+            for (int i = totalEstudiantes -1; i >= posicion ; i-=1) {
+                alumnos[i+1] = alumnos[1];
+            }
+
+            alumnos[posicion] = estudiante;
+            totalEstudiantes ++;
+        }
+
 
         public String toString(){
             return Arrays.toString(alumnos);
--- src/VectorTeste.java
+++ src/VectorTeste.java
@@ -6,13 +6,34 @@ public class VectorTeste {
 
         Vector lista = new Vector();
 
+        //Prueba tamaño lista
         System.out.println(lista.tamanio());
+        //Prueba agregar estudiante
         lista.agregar(estudiante1);
         System.out.println(lista.tamanio());
         lista.agregar(estudiante2);
         System.out.println(lista.tamanio());
 
         System.out.println(lista);
+        //Prueba metodo contiene
+        System.out.println(lista.contiene(estudiante1));
+        Estudiantes estudiante3 = new Estudiantes("Panchito");
+        System.out.println(lista.contiene(estudiante3));
+
+        //Prueba obtener estudiante
+        Estudiantes estudianteX = lista.obtener(1);
+        System.out.println(estudianteX);
+
+        //Prueba metodo adiciona, poner estudiante en una posicion
+        lista.adiciona(1,estudiante3);
+        System.out.println(lista);
+
+        //Prueba metodo quitar un estudiante
+        lista.eliminar(2);
+        System.out.println(lista);
+
+
+
 
     }
 }
